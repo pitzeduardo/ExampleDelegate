af4ad0708e8dd21da320ca760e1180a92c3c7d9b
Eduardo Pitz <42384045+pitzeduardo@users.noreply.github.com>
2018-08-15 19:20:49 -0300

    Example Using Delegate
    Example with ASP.NET Core 2.0 using with a simple way to use delegates.

---
+++ ExampleJSON/Program.cs
@@ -0,0 +1,53 @@
+﻿using Services;
+using Models;
+using System;
+
+namespace ExampleJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Using Delegate -");
+            var studentTest = MockStudentData();
+            var studentClass = new StudentService(studentTest);
+
+            Console.WriteLine("- Validate -");
+            studentClass.ValidadeStudent(StudentValidate);
+
+            Console.ReadLine();
+        }
+
+        public static bool StudentValidate(Student student)
+        {
+            Console.WriteLine("- Using StudentValidate -");
+
+            if (string.IsNullOrEmpty(student.Name))
+                return false;
+
+            if (string.IsNullOrEmpty(student.Email))
+                return false;
+
+            if (!student.Email.Contains("@"))
+                return false;
+
+            return true;
+        }
+
+        #region Fake Data
+
+        private static Student MockStudentData()
+        {
+            return new Student()
+            {
+                Name = "Eduardo Teste",
+                Address = "Rua Teste",
+                Email = "email@gmail.com",
+                Age = 20
+            };
+        }
+
+        #endregion 
+
+    }
+}
---
+++ ExampleJSON/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using System;
+
+namespace Services
+{
+    class StudentService
+    {
+        private Student _studentValidade;
+        public delegate bool ValidateHandler(Student student);
+
+        public StudentService(Student student)
+        {
+            _studentValidade = student;
+        }
+
+        public bool ValidadeStudent(ValidateHandler validateStudent)
+        {
+            return validateStudent(_studentValidade);
+        }
+
+    }
+}
